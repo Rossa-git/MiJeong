c099feecf4295c697efc53b4b708561bd1c0bd29
rossa <ruddybell@gamil.com>
2021-07-11 21:01:44 +0900

    20210711 스케줄러 인터페이스 정의

--- src/main/java/com/mijeong/scheduler/mapper/SchedulerMapper.java
+++ src/main/java/com/mijeong/scheduler/mapper/SchedulerMapper.java
@@ -1,5 +1,7 @@
 package com.mijeong.scheduler.mapper;
 
 public interface SchedulerMapper {
-
+	public int sendMessage(); //경매 종료 후 메세지
+	public int openAction(); //경매 시작
+	public int closeAction(); //경매 마감
 }
