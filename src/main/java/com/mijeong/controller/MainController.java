efdfc88e55616f4c5f34f0a189f1b23207758792
rossa <ruddybell@gamil.com>
2021-07-11 17:18:17 +0900

    20210711 메인페이지만 컨트롤러 연결

--- src/main/java/com/mijeong/controller/MainController.java
+++ src/main/java/com/mijeong/controller/MainController.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/main")
 public class MainController {
-
+	
+	@RequestMapping("/")
+	public String main() {
+		
+		return "main/mainPage";
+	}
 }
