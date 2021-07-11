4abd27a1cc2407ed34d2570885efd581e4a7d410
rossa <ruddybell@gamil.com>
2021-07-11 21:01:13 +0900

    20210711 스케쥴러 인터페이스 정의

--- src/main/java/com/mijeong/scheduler/service/SchedulerService.java
+++ src/main/java/com/mijeong/scheduler/service/SchedulerService.java
@@ -1,5 +1,7 @@
 package com.mijeong.scheduler.service;
 
 public interface SchedulerService {
-
+	public int sendMessage(); //경매 종료 후 메세지
+	public int openAction(); //경매 시작
+	public int closeAction(); //경매 마감
 }
--- src/main/java/com/mijeong/scheduler/service/SchedulerServiceImpl.java
+++ src/main/java/com/mijeong/scheduler/service/SchedulerServiceImpl.java
@@ -5,4 +5,19 @@ import org.springframework.stereotype.Service;
 @Service("schedulerService")
 public class SchedulerServiceImpl implements SchedulerService {
 
+	@Override
+	public int sendMessage() {
+		return 0;
+	}
+
+	@Override
+	public int openAction() {
+		return 0;
+	}
+
+	@Override
+	public int closeAction() {
+		return 0;
+	}
+
 }
